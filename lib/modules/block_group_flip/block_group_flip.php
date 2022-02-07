61d345bf395a42bfacf04f882c572b601c9b0627
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-02-07 16:17:00 +0100

    FEAT - ADD - STYLE FLIP

---
+++ lib/modules/block_group_flip/block_group_flip.php
@@ -0,0 +1,72 @@
+<?php
+	namespace sv100_premium;
+
+	class block_group_flip extends modules {
+		public function init(): block_group_flip {
+			$this->set_section_title( __( 'Block Group Style: Flip', 'sv100_premium' ) )
+				->set_section_type( 'settings' )
+				->load_settings()
+				->get_root()->add_section( $this );
+
+			if($this->is_active()){
+				$this->block_styles()->register_scripts();
+				add_action('wp', array($this, 'enqueue_scripts'));
+			}
+
+			return $this;
+		}
+
+		protected function load_settings(): block_group_flip {
+			$this->get_setting( 'activate' )
+				->set_title( __( 'Enable Style: Flip Group Block', 'sv100' ) )
+				->set_description( __( 'Group Block with two Child-Group-Blocks flips to second child when click/hover on first child.', 'sv100' ) )
+				->load_type( 'checkbox' );
+
+			return $this;
+		}
+
+		public function is_active(): bool{
+			// activate not set
+			if(!$this->get_setting('activate')->get_data()){
+				return false;
+			}
+			// activate not true
+			if($this->get_setting('activate')->get_data() !== '1'){
+				return false;
+			}
+
+			return true;
+		}
+
+		protected function register_scripts(): block_group_flip {
+			$this->get_script( 'style_flip' )
+				->set_is_gutenberg()
+				->set_path( 'lib/css/common/style_flip.css' );
+
+			return $this;
+		}
+
+		public function enqueue_scripts(): block_group_flip {
+			if(!$this->has_block_frontend('group')){
+				return $this;
+			}
+
+			foreach($this->get_scripts() as $script){
+				$script->set_is_enqueued();
+			}
+
+			return $this;
+		}
+
+		protected function block_styles(): block_group_flip {
+			$this->get_script('block')
+				->set_path('lib/js/backend/block_extra_styles.js')
+				->set_type('js')
+				->set_is_gutenberg()
+				->set_is_backend()
+				->set_deps(array('wp-blocks', 'wp-dom'))
+				->set_is_enqueued();
+
+			return $this;
+		}
+	}
--- lib/modules/modules.php
+++ lib/modules/modules.php
@@ -10,6 +10,7 @@
 			$this->load_module('block_group_section_visibility');
 			$this->load_module('block_group_sticky');
 			$this->load_module('block_group_toggle');
+			$this->load_module('block_group_flip');
 			$this->load_module('custom_lightbox');
 		}
 	}
