03556a00a6c527811dc6210dd9e3857cb8a98e6d
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-02-22 16:04:06 +0100

    UPDATE CORE FEAT - ADD - UNLIMITED LIGHTBOXES

--- lib/modules/custom_lightbox/custom_lightbox.php
+++ lib/modules/custom_lightbox/custom_lightbox.php
@@ -22,13 +22,39 @@
 				->set_description( __( 'Show Lightbox when linking to an anchor for Group with Hidden Style', 'sv100' ) )
 				->load_type( 'checkbox' );
 
-			$this->get_setting( 'width' )
+			$this->get_setting( 'lightbox' )
+				->set_title( __( 'Add a new lightbox', 'sv100' ) )
+				->load_type( 'group' );
+
+			$this->get_setting( 'lightbox' )
+				->run_type()
+				->add_child()
+				->set_ID( 'entry_label' )
+				->set_title( __( 'Label', 'sv100' ) )
+				->set_description( __( 'A label to differentiate your uploaded fonts.', 'sv100' ) )
+				->load_type( 'text' );
+
+			$this->get_setting( 'lightbox' )
+				->run_type()
+				->add_child()
+				->set_ID( 'entry_id' )
+				->set_title( __( 'ID', 'sv100' ) )
+				->set_description( __( 'Unique ID for Anchor/Link, allowed chars: A-Z, a-z, 0-9, _, -', 'sv100' ) )
+				->load_type( 'id' );
+
+			$this->get_setting( 'lightbox' )
+				->run_type()
+				->add_child()
+				->set_ID( 'width' )
 				->set_title( __( 'Lightbox Width', 'sv100' ) )
 				->set_is_responsive(true)
 				->set_default_value('75vw')
 				->load_type( 'text' );
 
-			$this->get_setting( 'height' )
+			$this->get_setting( 'lightbox' )
+				->run_type()
+				->add_child()
+				->set_ID( 'height' )
 				->set_title( __( 'Lightbox Height', 'sv100' ) )
 				->set_is_responsive(true)
 				->set_default_value('75vh')
@@ -62,9 +88,16 @@
 				->set_is_gutenberg()
 				->set_path( 'lib/css/common/style_lightbox_close_button.css' );
 
+			$selectors	= array();
+			foreach($this->get_setting( 'lightbox' )->get_data() as $lightbox){
+				$selectors[]		= "a[href='#".$lightbox['entry_id']."']";
+			}
+			$selector	= implode(',',$selectors);
+
 			$this->get_script( 'custom_lightbox_js' )
 				->set_type('js')
-				->set_path( 'lib/js/frontend/custom_lightbox.js' );
+				->set_path( 'lib/js/frontend/custom_lightbox.js' )
+				->set_localized(array('selector' => $selector));
 
 			add_filter( 'rocket_delay_js_exclusions', function ( $excluded_files = array() ) {
 				$excluded_files[] = '/lib/js/frontend/custom_lightbox.js';
--- lib/modules/custom_lightbox/lib/css/config/general.php
+++ lib/modules/custom_lightbox/lib/css/config/general.php
@@ -1,9 +1,26 @@
 <?php
 	// sticky style
-	echo $_s->build_css(
-		is_admin() ? '' : '.wp-block-group.is-style-sv-hidden.active > .wp-block-group__inner-container',
-		array_merge(
-			$module->get_setting('width')->get_css_data('width'),
-			$module->get_setting('height')->get_css_data('height')
-		)
-	);
+	$lightboxes		= $module->get_setting( 'lightbox' )->get_data();
+
+	if(is_array($lightboxes) && count($lightboxes) > 0) {
+		foreach ($module->get_setting('lightbox')->get_data() as $lightbox) {
+			$temp_width = clone $module->get_setting($lightbox['entry_id'].'_width');
+			$temp_height = clone $module->get_setting($lightbox['entry_id'].'_height');
+
+			echo $_s->build_css(
+				is_admin() ? '' : '.wp-block-group.is-style-sv-hidden.active#'.$lightbox['entry_id'].' > .wp-block-group__inner-container',
+				array_merge(
+					$temp_width
+						->set_is_responsive(true)
+						->load_type( 'text' )
+						->set_data($lightbox['width'])
+						->get_css_data('width'),
+					$temp_height
+						->set_is_responsive(true)
+						->load_type( 'text' )
+						->set_data($lightbox['height'])
+						->get_css_data('height')
+				)
+			);
+		}
+	}
