9727cc71372759936a66c01cda4ba8a56cebaee9
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-03-15 13:45:47 +0100

    fix CSS config generation

--- lib/modules/custom_lightbox/lib/css/config/general.php
+++ lib/modules/custom_lightbox/lib/css/config/general.php
@@ -8,30 +8,32 @@
 			$temp_height = clone $module->get_setting($lightbox['entry_id'] . '_height');
 			$temp_margin = clone $module->get_setting($lightbox['entry_id'] . '_margin');
 
-			echo $_s->build_css(
-				is_admin() ? '' :
-					(
-						'.wp-block-group.is-style-sv-hidden.active#' . $lightbox['entry_id'] . ' > .wp-block-group,' .
-						'.wp-block-group.is-style-sv-hidden.active#' . $lightbox['entry_id'] . ' > .wp-block-group__inner-container' // Fallback
-					),
-				array_merge(
-					$temp_width
-						->set_is_responsive(true)
-						->load_type('text')
-						->set_data($lightbox['width'])
-						->get_css_data('width'),
-					$temp_height
-						->set_is_responsive(true)
-						->load_type('text')
-						->set_data($lightbox['height'])
-						->get_css_data('height'),
-					$temp_margin
-						->set_is_responsive(true)
-						->load_type('margin')
-						->set_data($lightbox['margin'])
-						->get_css_data()
-				)
-			);
+			if(is_array($lightbox['width']) && is_array($lightbox['height']) && is_array($lightbox['margin'])) {
+				echo $_s->build_css(
+					is_admin() ? '' :
+						(
+							'.wp-block-group.is-style-sv-hidden.active#' . $lightbox['entry_id'] . ' > .wp-block-group,' .
+							'.wp-block-group.is-style-sv-hidden.active#' . $lightbox['entry_id'] . ' > .wp-block-group__inner-container' // Fallback
+						),
+					array_merge(
+						$temp_width
+							->set_is_responsive(true)
+							->load_type('text')
+							->set_data($lightbox['width'])
+							->get_css_data('width'),
+						$temp_height
+							->set_is_responsive(true)
+							->load_type('text')
+							->set_data($lightbox['height'])
+							->get_css_data('height'),
+						$temp_margin
+							->set_is_responsive(true)
+							->load_type('margin')
+							->set_data($lightbox['margin'])
+							->get_css_data()
+					)
+				);
+			}
 
 			if(is_array($lightbox['enable_background_layer'])) {
 				$properties = array();
